0dbd6771c8c08bfbdf39ca6acbfd9e20b6383813
Sosibona <empty.tara@gmail.com>
2019-09-25 14:01:23 +0300

    Sum

--- src/sum-of-odd.js
+++ src/sum-of-odd.js
@@ -25,7 +25,17 @@
  * @return {number}
  */
 function rowSumOddNumbers(row) {
-  // write code here
+  let startNumberOfRow = row * row - (row - 1);
+  let sum = startNumberOfRow;
+  if (row < 0) {
+    return 0;
+  } else {
+    for (let i = 1; i < row; i++) {
+      sum += (startNumberOfRow + 2);
+      startNumberOfRow += 2;
+    }
+  }
+  return sum;
 }
 
 module.exports = rowSumOddNumbers;
