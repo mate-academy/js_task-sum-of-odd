729cd2a725edbf2260fb2f4f3bfc35ebcfce3a3c
Senya Volkov <formobileprima@gmail.com>
2020-01-21 20:58:01 +0200

    wizard short solution

--- src/sum-of-odd.js
+++ src/sum-of-odd.js
@@ -11,31 +11,13 @@
  *    13    15    17    19
  * 21    23    25    27    29
  */
+
 function rowSumOddNumbers(row) {
   if (row !== Math.abs(row)) {
     return 0;
   }
 
-  let num = 0;
-
-  for (let i = 1; i < row; i++) {
-    for (let j = 0; j < i; j++) {
-      num += 2;
-    }
-  }
-
-  let firstDigit = num - 1;
-  const arr = [];
-
-  for (let i = 0; i < row; i++) {
-    firstDigit += 2;
-    arr.push(firstDigit);
-  }
-
-  const result = arr.reduce((a, b) => {
-    return a + b;
-  });
-
-  return result;
+  return Math.pow(row, 3);
 }
+
 module.exports = rowSumOddNumbers;
