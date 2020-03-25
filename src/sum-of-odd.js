62dde4642bbb5f26729b402d6265ced62a86edcf
Illia Oliynik <illiaoliynik@gmail.com>
2020-03-25 20:45:04 +0200

    solution

--- src/sum-of-odd.js
+++ src/sum-of-odd.js
@@ -2,27 +2,13 @@
 
 /**
  * Implement sumOfOdd function:
- *
  * Given the triangle of consecutive odd numbers:
- *
-             *             1
-             *          3     5
-             *       7     9    11
-             *    13    15    17    19
-             * 21    23    25    27    29
- *           31
- *          43
- *         57 59 61 63 65 67 69 71
  * Calculate the sum of all numbers in a row of the triangle
- *
  * Row index (starting at index 1) e.g.:
- *
  * rowSumOddNumbers(1); // 1
  * rowSumOddNumbers(2); // 3 + 5 = 8
  * rowSumOddNumbers(-1); // 0
- *
  * @param {number} row
- *
  * @return {number}
  */
 function rowSumOddNumbers(row) {
