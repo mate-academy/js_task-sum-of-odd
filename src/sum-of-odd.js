294f8348efd296868c29725270429ee21486de78
Nastia-rosy <nastiiarosy@gmail.com>
2020-03-27 14:09:03 +0200

    Solution

--- src/sum-of-odd.js
+++ src/sum-of-odd.js
@@ -25,7 +25,14 @@
  * @return {number}
  */
 function rowSumOddNumbers(row) {
-  // write code here
+  const firstNumber = row * row - row + 1;
+  let sum = 0;
+
+  for (let i = 0; i < row; i++) {
+    sum += firstNumber + 2 * i;
+  }
+
+  return sum;
 }
 
 module.exports = rowSumOddNumbers;
