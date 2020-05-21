81fbdf2463f7854ed72da69f3d39c5c6870a18d3
DTR <t.terry.rice@gmail.com>
2020-05-21 15:36:03 +0300

    final 1.0

--- src/sum-of-odd.js
+++ src/sum-of-odd.js
@@ -25,7 +25,11 @@
  * @return {number}
  */
 function rowSumOddNumbers(row) {
-  // write code here
+  if (row <= 0) {
+    return 0;
+  };
+
+  return row * row * row;
 }
 
 module.exports = rowSumOddNumbers;
